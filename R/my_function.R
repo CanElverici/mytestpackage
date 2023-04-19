077e014f20d9ac8e97e1b0903e8c25adaeb593c9
CanElverici <elverici.can@gmail.com>
2023-04-18 19:33:58 -0500

    initial package setup

---
+++ R/my_function.R
@@ -0,0 +1,13 @@
+#' My example function
+#'
+#' This function does something interesting
+#'
+#' @param x A numeric vector
+#' @return  The sum of x
+#' @export
+#'
+#' @examples
+#' my_function(1:10)
+my_function <- function(x) {
+  sum(x)
+}
